ea04e8d833aba954abb706880c917f885000f846
DracoGilga <cesar96707@gmail.com>
2024-12-18 15:41:14 -0600

    Add Character model

---
+++ Models/character.js
@@ -0,0 +1,10 @@
+class Character {
+    constructor(id, name, status, gender) {
+        this.id = id;
+        this.name = name;
+        this.status = status;
+        this.gender = gender;
+    }
+}
+
+module.exports = Character;
